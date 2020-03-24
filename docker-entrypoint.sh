5b8ced3277d2eef243afd0dfd535fc23e32eef68
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-03-24 13:03:26 +0100

    initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+export APPLICATION_NAME=$(cat "${K8S_SERVICEACCOUNT_PATH}/namespace")
+
+jupyter lab --notebook-dir=/home/${NB_USER} --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
