2198c2b51eeb697c928bb4d077ff1f7ffddc5bab
Erik Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-10-26 11:16:38 +0100

    add odbc lib to jupyter r image

--- jupyterhub-r/install_packages.R
+++ jupyterhub-r/install_packages.R
@@ -3,6 +3,7 @@ library(devtools)
 # Install packages from CRAN-r
 install.packages('listviewer', repos = 'https://cran.uib.no')
 install.packages('plotly', repos = 'https://cran.uib.no')
+install.packages('odbc', repos = 'https://cran.uib.no')
 
 # Install dataverkr
 install('dataverkr')
