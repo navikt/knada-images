70dba2dd39bd712f0913cb4732351f9bc88bdeed
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-01-28 22:38:48 +0100

    jupyterhub-r image: install R 4.0.3, install R packages

--- jupyterhub-r/entrypoint.sh
+++ jupyterhub-r/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "HTTPS_PROXY=$HTTPS_PROXY" >> /opt/conda/lib/R/etc/Renviron
-echo "https_proxy=$https_proxy" >> /opt/conda/lib/R/etc/Renviron
-echo "NO_PROXY=$NO_PROXY" >> /opt/conda/lib/R/etc/Renviron
-echo "no_proxy=$no_proxy" >> /opt/conda/lib/R/etc/Renviron
+echo "HTTPS_PROXY=$HTTPS_PROXY" >> /usr/lib/R/etc/Renviron
+echo "https_proxy=$https_proxy" >> /usr/lib/R/etc/Renviron
+echo "NO_PROXY=$NO_PROXY" >> /usr/lib/R/etc/Renviron
+echo "no_proxy=$no_proxy" >> /usr/lib/R/etc/Renviron
 
 tini -g -- start-notebook.sh
