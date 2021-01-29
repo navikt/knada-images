8b702ce8143973e522b678fd3b0a9c16fd9882c7
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-01-29 14:26:25 +0100

    add fable.prophet to jupyterhub-r image

--- jupyterhub-r/entrypoint.sh
+++ jupyterhub-r/entrypoint.sh
@@ -4,5 +4,8 @@ echo "HTTPS_PROXY=$HTTPS_PROXY" >> /usr/lib/R/etc/Renviron
 echo "https_proxy=$https_proxy" >> /usr/lib/R/etc/Renviron
 echo "NO_PROXY=$NO_PROXY" >> /usr/lib/R/etc/Renviron
 echo "no_proxy=$no_proxy" >> /usr/lib/R/etc/Renviron
+echo "DATAVERK_BUCKET_ENDPOINT=$DATAVERK_BUCKET_ENDPOINT" >> /usr/lib/R/etc/Renviron
+echo "DATAVERK_API_ENDPOINT=$DATAVERK_API_ENDPOINT" >> /usr/lib/R/etc/Renviron
+echo "DATAVERK_ES_HOST=$DATAVERK_ES_HOST" >> /usr/lib/R/etc/Renviron
 
 tini -g -- start-notebook.sh
