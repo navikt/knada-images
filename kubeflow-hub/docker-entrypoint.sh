36055085b8b0f552c86794be787a84082191bb4a
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-04-30 08:49:39 +0200

    remove unused images

--- kubeflow-hub/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-export APPLICATION_NAME=$(cat "${K8S_SERVICEACCOUNT_PATH}/namespace")
-
-jupyter notebook --notebook-dir=/home/${NB_USER} --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
--- kubeflow-lab/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p /home/jovyan/.local/share/code-server/extensions/ms-python.python
-cp -r ../ms-python.python/* /home/${NB_USER}/.local/share/code-server/extensions/ms-python.python
-
-export APPLICATION_NAME=$(cat "${K8S_SERVICEACCOUNT_PATH}/namespace")
-
-jupyter lab --notebook-dir=/home/${NB_USER} --ip=0.0.0.0 --no-browser --allow-root --port=8888 --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.allow_origin='*' --NotebookApp.base_url=${NB_PREFIX}
