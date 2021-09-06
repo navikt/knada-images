90b21c9d917f6d9be5ceb1a3b1a042d63079f015
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-09-06 18:53:39 +0200

    entrypoint

--- jupyterhub/nav-entrypoint.sh
+++ jupyterhub/nav-entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 mkdir -p /home/jovyan/.local/share/code-server/extensions/
-mv /home/ms-python.python /home/jovyan/.local/share/code-server/extensions/ms-python.python
+cp -r /home/ms-python.python/* /home/${NB_USER}/.local/share/code-server/extensions/ms-python.python
 
 exec "$@"
