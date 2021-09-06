2183a6605863f23deec66593e4ddacb05f9d99a8
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-09-06 18:04:40 +0200

    entrypoint

--- jupyterhub/nav-entrypoint.sh
+++ jupyterhub/nav-entrypoint.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-bsdtar -xvf vspackage - extension
 mkdir -p /home/jovyan/.local/share/code-server/extensions/
-mv extension /home/jovyan/.local/share/code-server/extensions/ms-python.python
+mv /home/ms-python.python /home/jovyan/.local/share/code-server/extensions/ms-python.python
 
 exec "$@"
