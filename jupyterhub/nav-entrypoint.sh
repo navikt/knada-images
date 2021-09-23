fa94a7401dc538129ce6ffb054f2ae2daeac0ea6
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-09-23 08:30:16 +0200

    revert to using default entrypoint

--- jupyterhub/nav-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir -p /home/jovyan/.local/share/code-server/extensions/
-cp -r /home/ms-python.python/* /home/${NB_USER}/.local/share/code-server/extensions/ms-python.python
-
-exec "$@"
