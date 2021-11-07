1f9f2a76cc606ecf7c34a9966e67be849a8426c7
Erik Vattekar <erik.vattekar@nav.no>
2021-11-07 21:56:48 +0100

    r-image: upgrade rstudio server to 2021.09.0-351, set default reticulate python version

--- jupyterhub-r/entrypoint.sh
+++ jupyterhub-r/entrypoint.sh
@@ -7,8 +7,8 @@ echo "no_proxy=$no_proxy" >> /usr/lib/R/etc/Renviron
 echo "DATAVERK_BUCKET_ENDPOINT=$DATAVERK_BUCKET_ENDPOINT" >> /usr/lib/R/etc/Renviron
 echo "DATAVERK_API_ENDPOINT=$DATAVERK_API_ENDPOINT" >> /usr/lib/R/etc/Renviron
 echo "DATAVERK_ES_HOST=$DATAVERK_ES_HOST" >> /usr/lib/R/etc/Renviron
-echo "DATAVERK_HOST=$DATAVERK_HOST" >> /usr/lib/R/etc/renviron
-echo "DATAVERK_ENVIRONMENT=$DATAVERK_ENVIRONMENT" >> /usr/lib/R/etc/renviron
+echo "DATAVERK_HOST=$DATAVERK_HOST" >> /usr/lib/R/etc/Renviron
+echo "DATAVERK_ENVIRONMENT=$DATAVERK_ENVIRONMENT" >> /usr/lib/R/etc/Renviron
 echo "TZ=$TZ" >> /usr/lib/R/etc/Renviron
 
 tini -g -- start-notebook.sh
