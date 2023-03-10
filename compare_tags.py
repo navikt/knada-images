9adb7f095b22195e6f660227099c154d422e8618
Erik Vattekar <erik.vattekar@nav.no>
2023-03-10 12:01:42 +0100

    Build jupyter image on updates to base-notebook
    - Add step to workflow comparing last updated for jupyter/base-notebook
      with knada jupyter images and trigger rebuild when we are using
    outdated parent image for each python version tag
    - Run workflow periodically in addition to on push and on
      workflow_dispatch

--- compare_tags.py
+++ compare_tags.py
@@ -4,19 +4,22 @@ import requests
 
 from datetime import date
 
-status_image_up_to_date = 0
-status_something_wrong = 1
-status_image_needs_build = 100
+JUPYTER_REPO_URL_PREFIX = "https://hub.docker.com/v2/namespaces/jupyter/repositories/base-notebook/tags"
+
+# Return values from script
+STATUS_IMAGE_UP_TO_DATE_WITH_PARENT = 0
+STATUS_ERROR = 1
+STATUS_IMAGE_OUTDATED = 100
 
 def get_last_knada_image(last_knada: list) -> date:
     if not len(last_knada) == 1:
         print("list of last knada images should only contain one element")
-        exit(status_something_wrong)
+        exit(STATUS_ERROR)
 
     return date.fromisoformat(last_knada[0]["updateTime"][:10])
 
 def get_last_jupyter_image(image_tag: str) -> date:
-    res = requests.get(f"https://hub.docker.com/v2/namespaces/jupyter/repositories/base-notebook/tags/{image_tag}")
+    res = requests.get(f"{JUPYTER_REPO_URL_PREFIX}/{image_tag}")
     res.raise_for_status()
 
     data = res.json()
@@ -27,6 +30,8 @@ if __name__ == "__main__":
     date_jupyter = get_last_jupyter_image(sys.argv[2])
 
     if date_knada < date_jupyter:
-        exit(status_image_needs_build)
-    
-    exit(status_image_up_to_date) 
+        print("Knada image is outdated")
+        exit(STATUS_IMAGE_OUTDATED)
+
+    print("Knada image is up to date with parent")
+    exit(STATUS_IMAGE_UP_TO_DATE_WITH_PARENT) 
